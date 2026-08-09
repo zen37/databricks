aa3db4aa5a7549207a3f5f9b5d028759d8f2101e
zen37 <mobi2009ro@gmail.com>
2026-08-09 14:13:47 -0700

    Clean up list_scopes_secrets.py by removing comments
    Remove commented-out lines for better readability.

--- 1-secrets/list_scopes_secrets.py
+++ 1-secrets/list_scopes_secrets.py
@@ -1,7 +1,3 @@
-print(dbutils.secrets.listScopes())
-print(dbutils.secrets.list("massive"))
-print(dbutils.secrets.list("database"))
-print("------")
 for scope in dbutils.secrets.listScopes():
     for secret in dbutils.secrets.list(scope.name):
         print(f"{scope.name}  →  {secret.key}")
